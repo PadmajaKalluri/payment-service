5025c776d1763d990ea93f449f9c3b815aefd929
PadmajaKalluri <padmaja.kal@gmail.com>
2026-03-17 06:20:38 +0200

    Update PaymentController.java

--- src/main/java/com/example/paymentservice/controller/PaymentController.java
+++ src/main/java/com/example/paymentservice/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package com.example.paymentservice.controller;
 import com.example.paymentservice.entity.Payment;
+import com.example.paymentservice.dto.PaymentRequest;
 import com.example.paymentservice.service.PaymentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,9 +13,9 @@ public class PaymentController {
     private final PaymentService service;
     public PaymentController(PaymentService service){ this.service = service; }
 
-    @PostMapping
-    public ResponseEntity<Payment> makePayment(@RequestBody Payment payment){
-        return ResponseEntity.status(201).body(service.makePayment(payment));
+    @PostMapping("/makePayment")
+    public ResponseEntity<Payment> makePayment(@RequestBody PaymentRequest paymentRequest){
+        return ResponseEntity.status(201).body(service.makePayment(paymentRequest));
     }
 
     @GetMapping("/{id}")
